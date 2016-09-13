85923bf84965952f884ec694b2a2c0adcc1958ab
PauloGaldo <hades_54st@hotmail.com>
2016-09-12 23:25:53 -0300

    Vistas de login movidas y control de usuario agregado

---
+++ Vistas/Login/FormLogin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClasesBase;
+using Vistas.Login;
+
+namespace Vistas.Login
+{
+    /// <summary>
+    /// Lógica de interacción para FormLogin.xaml
+    /// </summary>
+    public partial class FormLogin : UserControl
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        public String Username
+        {
+            get { return txtUser.Text; }
+        }
+
+        public String Password
+        {
+            get { return txtPassword.Password; }
+        }
+    }
+}
