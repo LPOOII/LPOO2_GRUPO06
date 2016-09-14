9d64f6ce4146cad63d501323db365236305d7467
PauloGaldo <hades_54st@hotmail.com>
2016-09-13 23:42:55 -0300

    metodos agregados para login de usuario

--- Vistas/Login/FormLogin.xaml.cs
+++ Vistas/Login/FormLogin.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections;
 using ClasesBase;
 using Vistas.Login;
 
@@ -21,19 +22,75 @@ namespace Vistas.Login
     /// </summary>
     public partial class FormLogin : UserControl
     {
+        /// <summary>
+        /// variables de control de login
+        /// </summary>
+        bool isLogged = false;        
+        private ArrayList usuarios = new ArrayList();
         public FormLogin()
         {
             InitializeComponent();
+            usuarios.Add(new Usuario("admin", "1111", 1));
+            usuarios.Add(new Usuario("mozo", "2222", 2));
+            usuarios.Add(new Usuario("cajero", "3333", 3));
+            usuarios.Add(new Usuario("repositor", "4444", 4));
+            usuarios.Add(new Usuario("pastelero", "5555", 5));
+            usuarios.Add(new Usuario("comprador", "6666", 6));
+            usuarios.Add(new Usuario("vendedor", "7777", 7));
         }
 
-        public String Username
+        /// <summary>
+        /// getter para vereficar si usuario esta logeado
+        /// </summary>
+        public bool isLogeado
         {
-            get { return txtUser.Text; }
+            get { return isLogged; }        
         }
 
-        public String Password
+        /// <summary>
+        /// Author Paulo
+        /// metodo encargado del login de usuario
+        /// </summary>
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            get { return txtPassword.Password; }
+            login();
         }
+
+        /// <summary>
+        /// metodo de login de usuarios
+        /// </summary>
+        private void login()
+        {
+            foreach (Usuario usuario in usuarios)
+            {
+                if (usuario.Usr_UserName == txtUser.Text && usuario.Usr_Password == txtPassword.Password)
+                {
+                    this.isLogged = true;
+                    var myWindow = Window.GetWindow(this);
+                    LoginWindow.UsuarioLogeado = usuario;
+                    myWindow.Close();
+                    break;
+                }
+            }
+            if (!isLogged)
+            {
+                MessageBox.Show("Password y/o usuario incorrecto");
+            }
+
+        }
+
+        /// <summary>
+        /// metodo para ejecutar el login presionando la tecla enter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                login();
+            }
+        }
+        
     }
 }
