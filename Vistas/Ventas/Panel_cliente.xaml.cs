bba46e15e4eb3af73414990c284b7ca88269a2c5
PauloGaldo <hades_54st@hotmail.com>
2016-09-24 22:35:18 -0300

    Metodos agregar y actualizar implementados

--- Vistas/Ventas/Panel_cliente.xaml.cs
+++ Vistas/Ventas/Panel_cliente.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using ClasesBase;
+using System.Data;
 
 namespace Vistas
 {
@@ -19,23 +20,58 @@ namespace Vistas
     /// </summary>
     public partial class Panel_cliente : Window
     {
+        bool saveOrUpdate = false;
+
         public Panel_cliente()
         {
             InitializeComponent();
+            DataTable dtClientes = ClienteModel.list_clientes();
+            dataGridClientes.ItemsSource = dtClientes.DefaultView;
         }
 
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
-            Cliente cliente = new Cliente();
-            cliente.Cli_Id = 1;
-            cliente.Cli_Nombre = nombreApellido.Text;
-            cliente.Cli_Domicilio = domicilio.Text;
-            cliente.Cli_Departamento = departamento.Text;
-            cliente.Cli_Codigo_Postal = codigoPostal.Text;
-            cliente.Cli_Email = email.Text;
-            cliente.Cli_Telefono = telefono.Text;
-            cliente.Cli_Fecha_Nac = fechaNacimiento.SelectedDate.Value.Date;
-            dataGridClientes.Items.Add(cliente);
+            try
+            {
+                Cliente cliente = new Cliente();
+                cliente.Cli_Id = Convert.ToInt32(idCliente.Text);
+                cliente.Cli_Nombre = nombreApellido.Text;
+                cliente.Cli_Domicilio = domicilio.Text;
+                cliente.Cli_Departamento = departamento.Text;
+                cliente.Cli_Codigo_Postal = codigoPostal.Text;
+                cliente.Cli_Email = email.Text;
+                cliente.Cli_Telefono = telefono.Text;
+                cliente.Cli_Fecha_Nac = fechaNacimiento.SelectedDate.Value.Date;
+                if (saveOrUpdate)
+                {
+                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Deseas guardar los cambios?", "Actualizar", System.Windows.MessageBoxButton.YesNo);
+                    if (messageBoxResult == MessageBoxResult.Yes)
+                    {
+                        ClienteModel.update_Cliente(cliente, cliente.Cli_Id);
+                        MessageBox.Show("Datos actualizados");
+                    }
+                }
+                else
+                {
+                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Deseas agregar nuevo cliente?", "Agregar", System.Windows.MessageBoxButton.YesNo);
+                    if (messageBoxResult == MessageBoxResult.Yes)
+                    {
+                        ClienteModel.insert_Cliente(cliente);
+                        MessageBox.Show("Cliente agregado");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if(ex is NullReferenceException || ex is InvalidOperationException)
+                {
+                    MessageBox.Show("Los campos no deben estar vacios");
+                }                
+            }
+            saveOrUpdate = false;
+            DataTable dtClientes = ClienteModel.list_clientes();
+            dataGridClientes.ItemsSource = dtClientes.DefaultView;
+            idCliente.Text = "";
             nombreApellido.Text = "";
             domicilio.Text = "";
             departamento.Text = "";
@@ -43,7 +79,6 @@ namespace Vistas
             codigoPostal.Text = "";
             telefono.Text = "";
             fechaNacimiento.SelectedDate = null;
-            MessageBox.Show(cliente.ToString());
         }
 
         private void idCliente_TextChanged(object sender, TextChangedEventArgs e)
@@ -52,6 +87,7 @@ namespace Vistas
                 Cliente clienteEncontrado = TrabajarCliente.buscarCliente(idCliente.Text);
                 if (clienteEncontrado != null)
                 {
+                    saveOrUpdate = true;
                     nombreApellido.Text = clienteEncontrado.Cli_Nombre;
                     domicilio.Text = clienteEncontrado.Cli_Domicilio;
                     departamento.Text = clienteEncontrado.Cli_Departamento;
@@ -62,6 +98,7 @@ namespace Vistas
                 }
                 else 
                 {
+                    saveOrUpdate = false;
                     nombreApellido.Text = "";
                     domicilio.Text = "";
                     departamento.Text = "";
