8a624ee9bc7ebd2b88f62a3db6d0ff9efc125433
PauloGaldo <hades_54st@hotmail.com>
2016-09-24 22:42:36 -0300

    Comentarios agregados

--- Vistas/Ventas/Panel_cliente.xaml.cs
+++ Vistas/Ventas/Panel_cliente.xaml.cs
@@ -25,14 +25,22 @@ namespace Vistas
         public Panel_cliente()
         {
             InitializeComponent();
+            // Inicializamos tabla de clientes en la vista.
             DataTable dtClientes = ClienteModel.list_clientes();
             dataGridClientes.ItemsSource = dtClientes.DefaultView;
         }
 
+        /// <summary>
+        /// Metodo para guardar cliente (Insertar o actualizar)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
+            // bucle try catch, para evitar exceptions al dejar campos vacios o mal llenados
             try
             {
+                // objeto cliente 
                 Cliente cliente = new Cliente();
                 cliente.Cli_Id = Convert.ToInt32(idCliente.Text);
                 cliente.Cli_Nombre = nombreApellido.Text;
@@ -42,11 +50,13 @@ namespace Vistas
                 cliente.Cli_Email = email.Text;
                 cliente.Cli_Telefono = telefono.Text;
                 cliente.Cli_Fecha_Nac = fechaNacimiento.SelectedDate.Value.Date;
+                // bucle if para determinar si se esta actualizando o agregando
                 if (saveOrUpdate)
                 {
                     MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Deseas guardar los cambios?", "Actualizar", System.Windows.MessageBoxButton.YesNo);
                     if (messageBoxResult == MessageBoxResult.Yes)
                     {
+                        // operacion de actualizacion
                         ClienteModel.update_Cliente(cliente, cliente.Cli_Id);
                         MessageBox.Show("Datos actualizados");
                     }
@@ -56,6 +66,7 @@ namespace Vistas
                     MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Deseas agregar nuevo cliente?", "Agregar", System.Windows.MessageBoxButton.YesNo);
                     if (messageBoxResult == MessageBoxResult.Yes)
                     {
+                        // operacion de insersion
                         ClienteModel.insert_Cliente(cliente);
                         MessageBox.Show("Cliente agregado");
                     }
@@ -65,12 +76,16 @@ namespace Vistas
             {
                 if(ex is NullReferenceException || ex is InvalidOperationException)
                 {
+                    // exception por campos vacios o mal llenados
                     MessageBox.Show("Los campos no deben estar vacios");
                 }                
             }
+            // reestablecimiento de la variable para guardar o actualizar
             saveOrUpdate = false;
+            // actualizacion del datagrid
             DataTable dtClientes = ClienteModel.list_clientes();
             dataGridClientes.ItemsSource = dtClientes.DefaultView;
+            // limpieza de los campos
             idCliente.Text = "";
             nombreApellido.Text = "";
             domicilio.Text = "";
@@ -81,12 +96,20 @@ namespace Vistas
             fechaNacimiento.SelectedDate = null;
         }
 
+        /// <summary>
+        /// Metodo para detectar si el texto cambio del input de cliente_id
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void idCliente_TextChanged(object sender, TextChangedEventArgs e)
         {
+            // Bucle if para evitar busquedas con id valor a cero (0)
             if(idCliente.Text != "0"){
+                // busqueda del cliente por id
                 Cliente clienteEncontrado = TrabajarCliente.buscarCliente(idCliente.Text);
                 if (clienteEncontrado != null)
                 {
+                    // carga de datos en los textbox
                     saveOrUpdate = true;
                     nombreApellido.Text = clienteEncontrado.Cli_Nombre;
                     domicilio.Text = clienteEncontrado.Cli_Domicilio;
@@ -98,6 +121,7 @@ namespace Vistas
                 }
                 else 
                 {
+                    // limpieza de los campos en caso de nueva busqueda
                     saveOrUpdate = false;
                     nombreApellido.Text = "";
                     domicilio.Text = "";
