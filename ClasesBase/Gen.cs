88487099e20d344e24a4ac45fca47e1796158174
Eduardo <edutest@gmail.com>
2016-10-13 00:08:38 -0300

    TP5 = clase Gen contiene propiedad de cantidad y Descripcion

---
+++ ClasesBase/Gen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Gen
+    {
+        private int cant;
+
+        public int Cant
+        {
+            get { return cant; }
+            set { cant = value; }
+        }
+        private string descrip;
+
+        public string Descrip
+        {
+            get { return descrip; }
+            set { descrip = value; }
+        }
+
+       
+        public Gen()
+        { }
+    }
+}
